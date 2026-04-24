4cc91d28c20c35c2dc036f33dc3db9d124451775
shereengrace14 <shereengrace214@gmail.com>
2026-04-24 15:09:08 +0530

    Create donor.py

---
+++ donor.py
@@ -0,0 +1,6 @@
+def get_donors():
+    return [
+        {"name": "Ravi", "blood": "A+", "location": "Hyderabad"},
+        {"name": "Sita", "blood": "B+", "location": "Vijayawada"},
+        {"name": "Rahul", "blood": "O+", "location": "Vizag"}
+    ]
